ed0e6b7c45cf90c474c4ac07916c58e16fac1f82
Juan Pieres <jpieres@gmail.com>
2013-05-30 22:18:37 +0200

    Setup commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Test application: tiny_dribbble
+
+This is a simplified version of dribbble.com with only a few basic features:
+- users can upload shots.
+- shots are images that have to be cropped down to 400x300px.
+- a user can like other people's shots.
+
+The test application can be found at:
+[*tiny_dribbble*](http://tiny_dribbble.herokuapp.com/)
+by [Juan Pîeres](es.linkedin.com/in/juanpieres/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyDribbble::Application.config.secret_token = 'f2ec0fde7b97aff4efa9806e5f4212302c1da329c0e2534417caf38bffa86df78107ffddc4298b548ed1c40d6fffaac2068f45d29470116273642a7aec76760d'
