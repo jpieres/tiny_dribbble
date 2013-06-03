276b25163b3e6d2d6d24651b619d51cc763183e0
Juan Pieres <jpieres@gmail.com>
2013-06-04 01:04:58 +0200

    Shots layout.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,10 @@
 class SiteController < ApplicationController
   def home
+    if user_signed_in?
+      @shots = current_user.shots
+    else
+      @shots = Shot.all
+    end
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def flash_messages
+    return if flash.empty?
+    alert_classes = { error: "error", notice: "success", alert: "error" }
+
+    flash.collect do |name, msg|
+      content_tag :div, { class: "alert alert-#{alert_classes[name]}"} do
+        content_tag(:button, "\u00D7", type: "button", class: "close", "data-dismiss" => "alert") +
+          msg
+      end
+    end.join("\n").html_safe
+  end
 end
